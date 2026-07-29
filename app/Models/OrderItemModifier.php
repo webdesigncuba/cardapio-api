b869ca2dbb1968164de5039525277259b301e619
David Cordero <webdesigncuba@gmail.com>
2026-07-29 16:35:21 -0300

    fix: adiciona importação da classe ModifierOption em OrderItemModifier

--- app/Models/OrderItemModifier.php
+++ app/Models/OrderItemModifier.php
@@ -6,6 +6,7 @@ use Database\Factories\OrderItemModifierFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\ModifierOption;
 
 class OrderItemModifier extends Model
 {
